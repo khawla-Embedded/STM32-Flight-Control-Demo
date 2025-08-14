194512d29b4e625ed7ac7e3986aca8b6c6d6b1c2
khawla-Embedded <Sfaxi.khaoula@yahoo.com>
2025-08-14 17:11:02 +0200

    Update flight_data.h

--- Core/Core/flight_data.h
+++ Core/Core/flight_data.h
@@ -1,1 +1,12 @@
+// flight_data.h
+#ifndef FLIGHT_DATA_H
+#define FLIGHT_DATA_H
 
+typedef struct {
+    float pitch;     // Angle en radians
+    float roll;      // Angle en radians
+    float altitude;  // Altitude en mètres
+    uint32_t timestamp_ms;
+} FlightData;
+
+#endif
